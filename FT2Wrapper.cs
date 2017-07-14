eab1fff1a3d729ea3509cf5cf1f47bf796d099eb
WokenDream <tianqi.liu@mail.utoronto.ca>
2017-07-14 15:45:19 -0400

    Initial commit

---
+++ FT2Wrapper.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+using FT_HANDLE = System.UInt32;
+
+namespace RFSwitchLibrary
+{
+    enum FT_STATUS//:Uint32
+    {
+        FT_OK = 0,
+        FT_INVALID_HANDLE,
+        FT_DEVICE_NOT_FOUND,
+        FT_DEVICE_NOT_OPENED,
+        FT_IO_ERROR,
+        FT_INSUFFICIENT_RESOURCES,
+        FT_INVALID_PARAMETER,
+        FT_INVALID_BAUD_RATE,
+        FT_DEVICE_NOT_OPENED_FOR_ERASE,
+        FT_DEVICE_NOT_OPENED_FOR_WRITE,
+        FT_FAILED_TO_WRITE_DEVICE,
+        FT_EEPROM_READ_FAILED,
+        FT_EEPROM_WRITE_FAILED,
+        FT_EEPROM_ERASE_FAILED,
+        FT_EEPROM_NOT_PRESENT,
+        FT_EEPROM_NOT_PROGRAMMED,
+        FT_INVALID_ARGS,
+        FT_OTHER_ERROR
+    };
+
+    public static class FT
+    {
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_ListDevices(void* pvArg1, void* pvArg2, UInt32 dwFlags);	// FT_ListDevices by number only
+        public static unsafe void ListDevices(void* pvArg1, void* pvArg2, UInt32 dwFlags)
+        {
+            FT_STATUS ftStatus = FT_ListDevices(pvArg1, pvArg2, dwFlags);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_ListDevices failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_ListDevices(UInt32 pvArg1, void* pvArg2, UInt32 dwFlags);	// FT_ListDevcies by serial number or description by index only
+        public static unsafe void ListDevices(UInt32 pvArg1, void* pvArg2, UInt32 dwFlags)
+        {
+            FT_STATUS ftStatus = FT_ListDevices(pvArg1, pvArg2, dwFlags);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_ListDevices failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern FT_STATUS FT_Open(UInt32 uiPort, ref FT_HANDLE ftHandle);
+        public static unsafe void Open(UInt32 uiPort, ref FT_HANDLE ftHandle)
+        {
+            FT_STATUS ftStatus = FT_Open(uiPort, ref ftHandle);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_Open failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_OpenEx(void* pvArg1, UInt32 dwFlags, ref FT_HANDLE ftHandle);
+        public static unsafe void OpenEx(void* pvArg1, UInt32 dwFlags, ref FT_HANDLE ftHandle)
+        {
+            FT_STATUS ftStatus = FT_OpenEx(pvArg1, dwFlags, ref ftHandle);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_OpenEx failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern FT_STATUS FT_Close(FT_HANDLE ftHandle);
+        public static unsafe void Close(FT_HANDLE ftHandle)
+        {
+            FT_STATUS ftStatus = FT_Close(ftHandle);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_Close failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_Read(FT_HANDLE ftHandle, void* lpBuffer, UInt32 dwBytesToRead, ref UInt32 lpdwBytesReturned);
+        public static unsafe void Read(FT_HANDLE ftHandle, void* lpBuffer, UInt32 dwBytesToRead, ref UInt32 lpdwBytesReturned)
+        {
+            FT_STATUS ftStatus = FT_Read(ftHandle, lpBuffer, dwBytesToRead, ref lpdwBytesReturned);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_Read failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_Write(FT_HANDLE ftHandle, void* lpBuffer, UInt32 dwBytesToWrite, ref UInt32 lpdwBytesWritten);
+        public static unsafe void Write(FT_HANDLE ftHandle, void* lpBuffer, UInt32 dwBytesToWrite, ref UInt32 lpdwBytesWritten)
+        {
+            FT_STATUS ftStatus = FT_Write(ftHandle, lpBuffer, dwBytesToWrite, ref lpdwBytesWritten);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_Write failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetBaudRate(FT_HANDLE ftHandle, UInt32 dwBaudRate);
+        public static unsafe void SetBaudRate(FT_HANDLE ftHandle, UInt32 dwBaudRate)
+        {
+            FT_STATUS ftStatus = FT_SetBaudRate(ftHandle, dwBaudRate);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetBaudRate failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetDataCharacteristics(FT_HANDLE ftHandle, byte uWordLength, byte uStopBits, byte uParity);
+        public static unsafe void SetDataCharacteristics(FT_HANDLE ftHandle, byte uWordLength, byte uStopBits, byte uParity)
+        {
+            FT_STATUS ftStatus = FT_SetDataCharacteristics(ftHandle, uWordLength, uStopBits, uParity);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetDataCharacteristics failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetFlowControl(FT_HANDLE ftHandle, char usFlowControl, byte uXon, byte uXoff);
+        public static unsafe void SetFlowControl(FT_HANDLE ftHandle, char usFlowControl, byte uXon, byte uXoff)
+        {
+            FT_STATUS ftStatus = FT_SetFlowControl(ftHandle, usFlowControl, uXon, uXoff);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetFlowControl failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetDtr(FT_HANDLE ftHandle);
+        public static unsafe void SetDtr(FT_HANDLE ftHandle)
+        {
+            FT_STATUS ftStatus = FT_SetDtr(ftHandle);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetDtr failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_ClrDtr(FT_HANDLE ftHandle);
+        public static unsafe void ClrDtr(FT_HANDLE ftHandle)
+        {
+            FT_STATUS ftStatus = FT_ClrDtr(ftHandle);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_ClrDtr failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetRts(FT_HANDLE ftHandle);
+        public static unsafe void SetRts(FT_HANDLE ftHandle)
+        {
+            FT_STATUS ftStatus = FT_SetRts(ftHandle);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetRts failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_ClrRts(FT_HANDLE ftHandle);
+        public static unsafe void ClrRts(FT_HANDLE ftHandle)
+        {
+            FT_STATUS ftStatus = FT_ClrRts(ftHandle);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_ClrRts failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_GetModemStatus(FT_HANDLE ftHandle, ref UInt32 lpdwModemStatus);
+        public static unsafe void GetModemStatus(FT_HANDLE ftHandle, ref UInt32 lpdwModemStatus)
+        {
+            FT_STATUS ftStatus = FT_GetModemStatus(ftHandle, ref lpdwModemStatus);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_GetModemStatus failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetChars(FT_HANDLE ftHandle, byte uEventCh, byte uEventChEn, byte uErrorCh, byte uErrorChEn);
+        public static unsafe void SetChars(FT_HANDLE ftHandle, byte uEventCh, byte uEventChEn, byte uErrorCh, byte uErrorChEn)
+        {
+            FT_STATUS ftStatus = FT_SetChars(ftHandle, uEventCh, uEventChEn, uErrorCh, uErrorChEn);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetChars failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_Purge(FT_HANDLE ftHandle, UInt32 dwMask);
+        public static unsafe void Purge(FT_HANDLE ftHandle, UInt32 dwMask)
+        {
+            FT_STATUS ftStatus = FT_Purge(ftHandle, dwMask);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_Purge failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetTimeouts(FT_HANDLE ftHandle, UInt32 dwReadTimeout, UInt32 dwWriteTimeout);
+        public static unsafe void SetTimeouts(FT_HANDLE ftHandle, UInt32 dwReadTimeout, UInt32 dwWriteTimeout)
+        {
+            FT_STATUS ftStatus = FT_SetTimeouts(ftHandle, dwReadTimeout, dwWriteTimeout);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetTimeouts failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_GetQueueStatus(FT_HANDLE ftHandle, ref UInt32 lpdwAmountInRxQueue);
+        public static unsafe void GetQueueStatus(FT_HANDLE ftHandle, ref UInt32 lpdwAmountInRxQueue)
+        {
+            FT_STATUS ftStatus = FT_GetQueueStatus(ftHandle, ref lpdwAmountInRxQueue);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_GetQueueStatus failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetBreakOn(FT_HANDLE ftHandle);
+        public static unsafe void SetBreakOn(FT_HANDLE ftHandle)
+        {
+            FT_STATUS ftStatus = FT_SetBreakOn(ftHandle);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetBreakOn failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetBreakOff(FT_HANDLE ftHandle);
+        public static unsafe void SetBreakOff(FT_HANDLE ftHandle)
+        {
+            FT_STATUS ftStatus = FT_SetBreakOff(ftHandle);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetBreakOff failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_GetStatus(FT_HANDLE ftHandle, ref UInt32 lpdwAmountInRxQueue, ref UInt32 lpdwAmountInTxQueue, ref UInt32 lpdwEventStatus);
+        public static unsafe void GetStatus(FT_HANDLE ftHandle, ref UInt32 lpdwAmountInRxQueue, ref UInt32 lpdwAmountInTxQueue, ref UInt32 lpdwEventStatus)
+        {
+            FT_STATUS ftStatus = FT_GetStatus(ftHandle, ref lpdwAmountInRxQueue, ref lpdwAmountInTxQueue, ref lpdwEventStatus);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_GetStatus failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetEventNotification(FT_HANDLE ftHandle, UInt32 dwEventMask, void* pvArg);
+        public static unsafe void SetEventNotification(FT_HANDLE ftHandle, UInt32 dwEventMask, void* pvArg)
+        {
+            FT_STATUS ftStatus = FT_SetEventNotification(ftHandle, dwEventMask, pvArg);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetEventNotification failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_ResetDevice(FT_HANDLE ftHandle);
+        public static unsafe void ResetDevice(FT_HANDLE ftHandle)
+        {
+            FT_STATUS ftStatus = FT_ResetDevice(ftHandle);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_ResetDevice failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetDivisor(FT_HANDLE ftHandle, char usDivisor);
+        public static unsafe void SetDivisor(FT_HANDLE ftHandle, char usDivisor)
+        {
+            FT_STATUS ftStatus = FT_SetDivisor(ftHandle, usDivisor);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetDivisor failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_GetLatencyTimer(FT_HANDLE ftHandle, ref byte pucTimer);
+        public static unsafe void GetLatencyTimer(FT_HANDLE ftHandle, ref byte pucTimer)
+        {
+            FT_STATUS ftStatus = FT_GetLatencyTimer(ftHandle, ref pucTimer);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_GetLatencyTimer failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetLatencyTimer(FT_HANDLE ftHandle, byte ucTimer);
+        public static unsafe void SetLatencyTimer(FT_HANDLE ftHandle, byte pucTimer)
+        {
+            FT_STATUS ftStatus = FT_SetLatencyTimer(ftHandle, pucTimer);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetLatencyTimer failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_GetBitMode(FT_HANDLE ftHandle, ref byte pucMode);
+        public static unsafe void GetBitMode(FT_HANDLE ftHandle, ref byte pucMode)
+        {
+            FT_STATUS ftStatus = FT_GetBitMode(ftHandle, ref pucMode);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_GetBitMode failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetBitMode(FT_HANDLE ftHandle, byte ucMask, byte ucEnable);
+        public static unsafe void SetBitMode(FT_HANDLE ftHandle, byte ucMask, byte ucEnable)
+        {
+            FT_STATUS ftStatus = FT_SetBitMode(ftHandle, ucMask, ucEnable);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetBitMode failed");
+            }
+        }
+
+        [DllImport("FTD2XX.dll")]
+        static extern unsafe FT_STATUS FT_SetUSBParameters(FT_HANDLE ftHandle, UInt32 dwInTransferSize, UInt32 dwOutTransferSize);
+        public static unsafe void SetUSBParameters(FT_HANDLE ftHandle, UInt32 dwInTransferSize, UInt32 dwOutTransferSize)
+        {
+            FT_STATUS ftStatus = FT_SetUSBParameters(ftHandle, dwInTransferSize, dwOutTransferSize);
+            if (ftStatus != FT_STATUS.FT_OK)
+            {
+                throw new Exception("FT_SetUSBParameters failed");
+            }
+        }
+    }
+}
---
+++ RFSwitchController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+using FT_HANDLE = System.UInt32;
+
+namespace RFSwitchLibrary
+{
+    public class RFSwitchController
+    {
+        public const UInt32 FT_BAUD_300 = 300;
+        public const UInt32 FT_BAUD_600 = 600;
+        public const UInt32 FT_BAUD_1200 = 1200;
+        public const UInt32 FT_BAUD_2400 = 2400;
+        public const UInt32 FT_BAUD_4800 = 4800;
+        public const UInt32 FT_BAUD_9600 = 9600;
+        public const UInt32 FT_BAUD_14400 = 14400;
+        public const UInt32 FT_BAUD_19200 = 19200;
+        public const UInt32 FT_BAUD_38400 = 38400;
+        public const UInt32 FT_BAUD_57600 = 57600;
+        public const UInt32 FT_BAUD_115200 = 115200;
+        public const UInt32 FT_BAUD_230400 = 230400;
+        public const UInt32 FT_BAUD_460800 = 460800;
+        public const UInt32 FT_BAUD_921600 = 921600;
+
+        public const UInt32 FT_LIST_NUMBER_ONLY = 0x80000000;
+        public const UInt32 FT_LIST_BY_INDEX = 0x40000000;
+        public const UInt32 FT_LIST_ALL = 0x20000000;
+        public const UInt32 FT_OPEN_BY_SERIAL_NUMBER = 1;
+        public const UInt32 FT_OPEN_BY_DESCRIPTION = 2;
+
+        // Word Lengths
+        public const byte FT_BITS_8 = 8;
+        public const byte FT_BITS_7 = 7;
+        public const byte FT_BITS_6 = 6;
+        public const byte FT_BITS_5 = 5;
+
+        // Stop Bits
+        public const byte FT_STOP_BITS_1 = 0;
+        public const byte FT_STOP_BITS_1_5 = 1;
+        public const byte FT_STOP_BITS_2 = 2;
+
+        // Parity
+        public const byte FT_PARITY_NONE = 0;
+        public const byte FT_PARITY_ODD = 1;
+        public const byte FT_PARITY_EVEN = 2;
+        public const byte FT_PARITY_MARK = 3;
+        public const byte FT_PARITY_SPACE = 4;
+
+        // Flow Control
+        public const UInt16 FT_FLOW_NONE = 0;
+        public const UInt16 FT_FLOW_RTS_CTS = 0x0100;
+        public const UInt16 FT_FLOW_DTR_DSR = 0x0200;
+        public const UInt16 FT_FLOW_XON_XOFF = 0x0400;
+
+        // Purge rx and tx buffers
+        public const byte FT_PURGE_RX = 1;
+        public const byte FT_PURGE_TX = 2;
+
+        // Events
+        public const UInt32 FT_EVENT_RXCHAR = 1;
+        public const UInt32 FT_EVENT_MODEM_STATUS = 2;
+
+        public unsafe bool SwitchToPathFor(String EvalBoard)
+        {
+            const string ChannelA = "DLP232M A";
+            //const string ChannelB = "DLP232M B";
+            const byte Enable = 0x01;
+            const byte Mask = 0xff; // all 8 pins set to output
+
+            UInt32 NumDevs;
+            FT_HANDLE ftHandle = 0;
+
+            byte[] DataSet = { 0 };
+            switch (EvalBoard)
+            {
+                case "B":
+                    DataSet[0] = 1; // (00000001) for Barium
+                    break;
+                case "X":
+                    DataSet[0] = 2; // (00000010) for Xenon
+                    break;
+                default:
+                    throw new Exception(" \"B\" stands for Barium, and \"X\" stands for Xenon");
+            }
+            byte[] DataRead = { 0 };
+
+            try
+            {
+                FT.ListDevices(&NumDevs, null, FT_LIST_NUMBER_ONLY); // get number of devices connected
+                for (UInt32 i = 0; i < NumDevs; ++i) //find the correct channel for the chip
+                {
+                    byte[] DevName = new byte[64];
+
+                    fixed (byte* temp_name = DevName, data_set = DataSet, data_read = DataRead)
+                    {
+                        FT.ListDevices(i, temp_name, FT_LIST_BY_INDEX | FT_OPEN_BY_DESCRIPTION); //get the description of current device
+
+                        System.Text.ASCIIEncoding Enc = new System.Text.ASCIIEncoding();
+                        string DevInfo = Enc.GetString(DevName, 0, DevName.Length).Trim('\0');
+                        if (!DevInfo.Equals(ChannelA))
+                        {
+                            continue; // we only use channel A
+                        }
+
+                        do
+                        {
+                            Console.WriteLine(" {0} Not euqal", i);
+
+                            FT.OpenEx(temp_name, FT_OPEN_BY_DESCRIPTION, ref ftHandle); //open device by description
+                            FT.SetBitMode(ftHandle, Mask, Enable); //enter Bit Bang mode, and set all the 8 pins to output
+                            System.Threading.Thread.Sleep(50);
+
+                            FT_HANDLE NumByteWritten = 2, NumByteRead = 2;
+                            FT.Write(ftHandle, data_set, (UInt32)DataSet.Length, ref NumByteWritten); //set voltages on the 8 pins
+                            System.Threading.Thread.Sleep(50);
+
+                            FT.Read(ftHandle, data_read, (FT_HANDLE)DataRead.Length, ref NumByteRead); //read volts back from pins
+
+                            FT.Close(ftHandle);
+                        } while (DataRead[0] != DataSet[0]);
+                        Console.WriteLine(" {0} euqal", i);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
